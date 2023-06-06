18ba8815848795baf78e2f38af817037b07f7d88
Sudharsan <727821tuec235@skct.edu.in>
2023-06-06 11:55:28 +0530

    push

---
+++ Day5/src/main/java/com/example/Day5/Task4.java
@@ -0,0 +1,26 @@
+package com.example.Day5;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Task4 
+{
+    public static void main( String[] args )
+    {
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://j2store.net/free/");
+        driver.manage().window().maximize();
+        JavascriptExecutor js = (JavascriptExecutor) driver ;
+        js.executeScript("window.scrollBy(0,1000)", "");
+        driver.findElement(By.xpath("//*[@id=\"Mod112\"]/div/div/ul/li[1]/h4/a")).click();
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+        driver.quit();
+        
+    }
+}
